656adf98883c935c3f5fc1dc474e653bfbbbd65d
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-14 02:10:28 +0200

    student list ui

--- src/components/student/StudentsView.jsx
+++ src/components/student/StudentsView.jsx
@@ -29,23 +29,29 @@ const StudentsView = () => {
             <th>Last Name</th>
             <th>Email</th>
             <th>Departement</th>
-            <th>Actions</th>
+            <th colSpan={3}>Actions</th>
           </tr>
         </thead>
         <tbody className="text-center">
           {students.map((student, index) => (
             <tr key={student.id}>
-              <th scope="row" key={index}>
+              {/*  <th scope="row" key={index}>
                 {index + 1}
-              </th>
+              </th> */}
               <td>{student.id}</td>
               <td>{student.firstName}</td>
               <td>{student.lastName}</td>
               <td>{student.email}</td>
               <td>{student.department}</td>
-              <td>View</td>
-              <td>Update</td>
-              <td>Delete</td>
+              <td className="mx-2">
+                <button className="btn btn-info">View</button>
+              </td>
+              <td className="mx-2">
+              <button className="btn btn-warning">Update</button>
+              </td>
+              <td className="mx-2">
+                <button className="btn btn-danger">Delete</button>
+              </td>
             </tr>
           ))}
         </tbody>
